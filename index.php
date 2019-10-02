06771a05745ff46abfb119274db65024f5e7362e
Tiffany <tiffanyren@live.ca>
2019-10-01 19:27:20 -0700

    basic header and css

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Basic Page Needs
+  ================================================== -->
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>The Instagrammer</title>
+
+    <!-- Mobile Specific Metas
+  ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+  ================================================== -->
+    <link href="css/normalize.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <!-- <link href="css/home.css" rel="stylesheet"> -->
+
+    <!-- Favicons
+	================================================== -->
+    <link rel="shortcut icon" href="">
+
+</head>
+
+<body>
+
+        <section class="container_nav">
+                <div class="nav_left">
+                    <a href="index.html" id="site-title">The Instagrammer</a>
+                </div>
+                <div class="nav_right">
+                    <nav>
+                        <a href="browse.html">Browse</a>
+                        <!-- <a href="index.html#projects">Projects</a> -->
+                        <a href="login.html">My Account</a>
+                    </nav>
+                </div>
+            </section>
+
+
+
+</body>
+
+</html>
