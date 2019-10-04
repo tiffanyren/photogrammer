5e1cd2a0d52824213542230ab12861d0bb67377b
Tiffany <tiffanyren@live.ca>
2019-10-03 18:12:33 -0700

    complete index with placeholders

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
   ================================================== -->
     <link href="css/normalize.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
-    <!-- <link href="css/home.css" rel="stylesheet"> -->
+    <link href="css/index.css" rel="stylesheet">
 
     <!-- Favicons
 	================================================== -->
@@ -29,18 +29,41 @@
 
 <body>
 
-        <section class="container_nav">
-                <div class="nav_left">
-                    <a href="index.html" id="site-title">The Instagrammer</a>
-                </div>
-                <div class="nav_right">
-                    <nav>
-                        <a href="browse.html">Browse</a>
-                        <!-- <a href="index.html#projects">Projects</a> -->
-                        <a href="login.html">My Account</a>
-                    </nav>
-                </div>
-            </section>
+    <section class="container_nav">
+        <div class="nav_left">
+            <a href="index.php" id="site-title">The Photogrammer</a>
+        </div>
+        <div class="nav_right">
+            <nav>
+                <a href="browse.php">Browse</a>
+                <a href="login.php">My Account</a>
+            </nav>
+        </div>
+    </section>
+
+    <section class="container_home">
+		<img src="img/rawpixel-593602-unsplash.jpg" alt="showcase jewelry">
+		<div class="img_text">
+			<h1>DISCOVER</h1>
+			<p>the places most coveted by Instagram photographers</p>
+		</div>
+		<div class="container_buttons">
+			<a class="button_nav button_trans" href="browse.php">Browse Now</a>
+		</div>
+	</section>
+
+    <section class="container_prod">
+		<div class="block">
+			<a href="listing1.php"><img src="img/placeholder-square.png" alt="placeholder1">
+    </div>
+    <div class="block">
+			<a href="listing1.php"><img src="img/placeholder-square.png" alt="placeholder1">
+    </div>
+    <div class="block">
+			<a href="listing1.php"><img src="img/placeholder-square.png" alt="placeholder1">
+    </div>
+
+	</section>
 
 
 
