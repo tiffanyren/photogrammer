11baa3b9e1d8f9a968601223c4ae8b18a97cfffd
Tiffany <tiffanyren@live.ca>
2019-10-03 22:23:15 -0700

    fixed navbar, pictures added

--- index.php
+++ index.php
@@ -41,29 +41,34 @@
         </div>
     </section>
 
+
+    <!-- Primary Page Layout
+	================================================== -->
+    <!-- all content goes in here -->
+
     <section class="container_home">
-		<img src="img/rawpixel-593602-unsplash.jpg" alt="showcase jewelry">
-		<div class="img_text">
-			<h1>DISCOVER</h1>
-			<p>the places most coveted by Instagram photographers</p>
-		</div>
-		<div class="container_buttons">
-			<a class="button_nav button_trans" href="browse.php">Browse Now</a>
-		</div>
-	</section>
+        <img src="img/rawpixel-593602-unsplash.jpg" alt="showcase jewelry">
+        <div class="img_text">
+            <h1>DISCOVER</h1>
+            <p>the places most coveted by Instagram photographers</p>
+        </div>
+        <div class="container_buttons">
+            <a class="button_nav button_trans" href="browse.php">Browse Now</a>
+        </div>
+    </section>
 
     <section class="container_prod">
-		<div class="block">
-			<a href="listing1.php"><img src="img/placeholder-square.png" alt="placeholder1">
-    </div>
-    <div class="block">
-			<a href="listing1.php"><img src="img/placeholder-square.png" alt="placeholder1">
-    </div>
-    <div class="block">
-			<a href="listing1.php"><img src="img/placeholder-square.png" alt="placeholder1">
-    </div>
+        <div class="block">
+            <a href="listing1.php"><img src="img/antonina-bukowska-9Pq42qFP9vY-unsplash.jpg" alt="cherry blossoms 1">
+        </div>
+        <div class="block">
+            <a href="listing1.php"><img src="img/kyle-ryan-nIujk826wE0-unsplash.jpg" alt="cambie street bridge 1">
+          </div>
+        <div class="block">
+            <a href="listing1.php"><img src="img/nathan-shurr-lJhWhFnGHK8-unsplash.jpg" alt="cambie street bridge 2">
+        </div>
 
-	</section>
+    </section>
 
 
 
